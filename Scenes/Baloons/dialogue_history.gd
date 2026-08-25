a2f648a50892f4e76f3ab766918b1457c40ef284
MikhaelAdam <mikhaeladams737@gmail.com>
2026-08-25 15:01:25 +0700

    Use RichTextLabel for history

--- Scenes/Baloons/dialogue_history.gd
+++ Scenes/Baloons/dialogue_history.gd
@@ -9,24 +9,13 @@ func _ready() -> void:
 	)
 
 func _on_dialogue_label_started_typing() -> void:
-	var label = Label.new()
-	label.text = %CharacterLabel.text + ": " + %DialogueLabel.dialogue_line.text
-	label.add_theme_font_override("font", FONT)
-	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-	label.custom_minimum_size.x = 300.0 
 	await get_tree().create_timer(0.1).timeout
-	%DialogueContainer.add_child(label)
+	%DialogueContainer.text += str("[b]" + %CharacterLabel.text + "[/b] : " + %DialogueLabel.dialogue_line.text) + "\n"
 
 
-func _on_responses_menu_response_selected(response: Variant) -> void:
-	var label = Label.new()
-	label.text = str("You: " + response.text)
-	label.add_theme_font_override("font", FONT)
-	label.add_theme_color_override("font_color", Color.GREEN)
-	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-	label.custom_minimum_size.x = 300.0 
-	%DialogueContainer.add_child(label)
+func _on_responses_menu_response_selected(_response: Variant) -> void:
+	# use the color green for the player response
+	%DialogueContainer.text += str("[color=green][b]You[/b] : " + _response.text) + "[/color]\n"
 
 func clear_history() -> void:
-	for child in %DialogueContainer.get_children():
-		child.queue_free()
+	%DialogueContainer.text = ""
